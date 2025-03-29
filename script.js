db7ca51ae9bf97f8dafe2478916c383aee4a8922
ey35 <96440556+ey35@users.noreply.github.com>
2025-03-29 15:03:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const addButton = document.getElementById('addButton');
 const newPlaylistButton = document.getElementById('newPlaylistButton');
 const playlist = document.getElementById('playlist');
 const audioPlayer = document.getElementById('audioPlayer');
-const playlistSection = document.getElementById('playlistSection');
-const librarySection = document.getElementById('librarySection');
-const playlistTab = document.getElementById('playlistTab');
-const libraryTab = document.getElementById('libraryTab');
+const playPauseButton = document.getElementById('playPauseButton');
+const nextTrackButton = document.getElementById('nextTrackButton');
+const prevTrackButton = document.getElementById('prevTrackButton');
+
 let tracks = [];
-let playlists = [];
+let currentTrackIndex = 0;
+let isPlaying = false;
 
 addButton.addEventListener('click', () => {
     const files = fileInput.files;
@@ -25,17 +26,56 @@ addButton.addEventListener('click', () => {
 
 function updatePlaylist() {
     playlist.innerHTML = '';
-    tracks.forEach((track) => {
+    tracks.forEach((track, index) => {
         const trackElement = document.createElement('div');
         trackElement.className = 'track';
         trackElement.textContent = track.name;
         trackElement.addEventListener('click', () => {
-            audioPlayer.src = track.url;
-            audioPlayer.play();
+            currentTrackIndex = index;
+            playTrack();
         });
         playlist.appendChild(trackElement);
     });
 }
+
+function playTrack() {
+    if (tracks.length > 0) {
+        audioPlayer.src = tracks[currentTrackIndex].url;
+        audioPlayer.play();
+        isPlaying = true;
+        playPauseButton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        updateTrackDetails();
+    }
+}
+
+function updateTrackDetails() {
+    const currentTrack = tracks[currentTrackIndex];
+    const trackElement = document.querySelector('.track');
+    const artistElement = document.querySelector('.artist');
+    trackElement.textContent = currentTrack.name;
+    artistElement.textContent = "Artist Name"; // Placeholder for artist name
+}
+
+playPauseButton.addEventListener('click', () => {
+    if (isPlaying) {
+        audioPlayer.pause();
+        playPauseButton.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    } else {
+        audioPlayer.play();
+        playPauseButton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+    }
+    isPlaying = !isPlaying;
+});
+
+nextTrackButton.addEventListener('click', () => {
+    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+    playTrack();
+});
+
+prevTrackButton.addEventListener('click', () => {
+    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    playTrack();
+});
 
 newPlaylistButton.addEventListener('click', () => {
     const playlistName = prompt("Enter playlist name:");
@@ -47,18 +87,7 @@ newPlaylistButton.addEventListener('click', () => {
             alert("Feature to view/edit this playlist will be implemented.");
         });
         playlist.appendChild(newPlaylist);
-        playlists.push(playlistName);
     }
-});
-
-playlistTab.addEventListener('click', () => {
-    playlistSection.style.display = 'block';
-    librarySection.style.display = 'none';
-});
-
-libraryTab.addEventListener('click', () => {
-    playlistSection.style.display = 'none';
-    librarySection.style.display = 'block';
 });
 
 const checkbox = document.getElementById("checkbox");
